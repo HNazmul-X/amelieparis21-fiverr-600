9ce0b3d1eb013e3fdfe1a33faaa6fb781a8f9087
MasumBilla19 <masumiraj0218@gmail.com>
2022-01-01 07:47:29 +0600

    added verification

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import CardRoundImage from "../../assets/images/Group 23.png";
+import Verification from "../../assets/images/Group 22.png";
+import Successful from "../../assets/images/Group 1262.png";
 
 const SignUp = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
@@ -136,6 +138,52 @@ const SignUp = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="container">
+                {/* <div className="row">
+                    <div className="col-lg-5 col-md-7 col-sm-9 mx-auto">
+                        <div id="signUp_Verification">
+                            <div class="VCard">
+                                <img src={Verification} class="img-fluid" alt="" />
+                            </div>
+
+                            <div className="verificationDetails">
+                                <div className="verificationText">
+                                    <h5>VERIFICATION</h5>
+                                    <p>Email has been sent to you email. check the email and Provide a Correct OTP</p>
+                                </div>
+
+                                <form action="">
+                                    <div className="form-floating otpWrapper my-3">
+                                        <input type="number" min={0} maxlength="6" class="form-control primary-input otpInput" id="floatingInput" placeholder="6 Digit OTP" />
+                                        <label for="floatingInput">6 Digit OTP *</label>
+                                        
+                                    </div>
+
+                                    <button className="btn verifyBtn">Verify</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+            </div>
+
+            <div className="container">
+                {/* <div className="row">
+                    <div className="col-md-8 offset-md-2">
+                        <div id="successful_area">
+                            <div class="SCard">
+                                <img src={Successful} class="img-fluid" alt="" />
+                            </div>
+
+                            <div className="SInfo">
+                                <h4>Profile verified Successfully</h4>
+                                <p>Please wait for admin approve for continue with this account</p>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
             </div>
         </div>
     );
