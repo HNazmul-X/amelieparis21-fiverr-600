5a8ddef74d3b912f4a01a343536c15daed359ad1
programmeranamul <programmer.anamul@gmail.com>
2022-01-07 01:37:00 +0600

    setup profile button added

--- src/Dashboard/Pages/ProfileCreation/components/LeftSide.jsx
+++ src/Dashboard/Pages/ProfileCreation/components/LeftSide.jsx
@@ -6,32 +6,62 @@ import SetupSocialLink from "./SetupSocialLink";
 import SocialLinkSelection from "./SocialLinkSelection";
 import SetupProfilePics from "./SetupProfilePics";
 import { useProfileContext } from "../../../../Context/ProfileTemplateContext";
+import SetupProfileButton from './SetupProfileButton';
 
 function LeftSide() {
-    const { socialLinks } = useProfileContext();
-    const [open, setOpen] = useState([]);
-    const [iconPopupShow, setIconPopupShow] = useState(true);
-    const [selectedIcon, setSelectedIcon] = useState([]);
-    const handelOpen = (data) => {
-        if (open.includes(data)) {
-            const newOpen = open.filter((el) => el !== data);
-            setOpen(newOpen);
-        } else {
-            setOpen([...open, data]);
-        }
-    };
+  const { socialLinks } = useProfileContext();
+  const [open, setOpen] = useState([]);
+  const [iconPopupShow, setIconPopupShow] = useState(true);
+  const [selectedIcon, setSelectedIcon] = useState([]);
+  const handelOpen = (data) => {
+    if (open.includes(data)) {
+      const newOpen = open.filter((el) => el !== data);
+      setOpen(newOpen);
+    } else {
+      setOpen([...open, data]);
+    }
+  };
 
-    return (
-        <>
-            <div className="left-side-container db-template">
-                <SelectProfileTemplate handelOpen={handelOpen} open={open} title={"Select Your Profile Template"} />
-                <SetupProfilePics handelOpen={handelOpen} open={open} title={"Setup Profile Link"} />
-                <SetUpPersonalInformation handelOpen={handelOpen} open={open} title={"Setup Personal Information"} />
-                <SetupSocialLink setIconPopupShow={setIconPopupShow} handelOpen={handelOpen} open={open} title={"Setup Social Link"} />
-            </div>
-            {iconPopupShow ? <SocialLinkSelection selectedIcon={selectedIcon} setSelectedIcon={setSelectedIcon} onClose={() => setIconPopupShow(!iconPopupShow)} /> : null}
-        </>
-    );
+  return (
+    <>
+      <div className="left-side-container db-template">
+        <SelectProfileTemplate
+          handelOpen={handelOpen}
+          open={open}
+          title={"Select Your Profile Template"}
+        />
+        <SetupProfilePics
+          handelOpen={handelOpen}
+          open={open}
+          title={"Setup Profile Link"}
+        />
+        <SetUpPersonalInformation
+          handelOpen={handelOpen}
+          open={open}
+          title={"Setup Personal Information"}
+        />
+        <SetupSocialLink
+          setIconPopupShow={setIconPopupShow}
+          handelOpen={handelOpen}
+          open={open}
+          title={"Setup Social Link"}
+        />
+        <SetupProfileButton
+          setIconPopupShow={setIconPopupShow}
+          handelOpen={handelOpen}
+          open={open}
+          title={"Setup Profile Button info"}
+        />
+      </div>
+      {iconPopupShow ? (
+        <SocialLinkSelection
+          selectedIcon={selectedIcon}
+          setSelectedIcon={setSelectedIcon}
+          onClose={() => setIconPopupShow(!iconPopupShow)}
+        />
+      ) : null}
+    </>
+  );
 }
 
 export default LeftSide;
