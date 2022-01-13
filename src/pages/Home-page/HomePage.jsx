62fe23c31b52178baa1e393d435665486324b5a7
programmeranamul <programmer.anamul@gmail.com>
2022-01-14 03:30:30 +0600

    home page video section added

---
+++ src/pages/Home-page/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MultipleDesign from './sections/MultipleDesign'
+import VideoExplainer from './sections/VideoExplainer';
+
+function HomePage() {
+    return (
+        <><div className='py-5'>
+
+        </div>
+            <MultipleDesign />
+            <VideoExplainer />
+            <div className='py-5'>
+
+        </div>
+        </>
+    )
+}
+
+export default HomePage
