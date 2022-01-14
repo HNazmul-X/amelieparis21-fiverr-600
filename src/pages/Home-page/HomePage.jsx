5bd54cca055a37df585b26fdcee355050ba6b10d
Al Mahamud Murad <muradhasan.7845@gmail.com>
2022-01-14 14:56:27 +0600

    homePage1stSection

---
+++ src/pages/Home-page/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./HomeContainer/HeroSection";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+};
+
+export default HomePage;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -20,45 +20,55 @@ import SingleCardPreview from "../Dashboard/Pages/CardRequeste/Releted/SingleCar
 import AllRequest from "../Dashboard/Pages/ProfileRequest/Subpages/AllRequest/AllRequest";
 import ProfileCreation from "../Dashboard/Pages/ProfileRequest/Subpages/ProfileCreation/ProfileCreation";
 import PlayGround from "../pages/playgournd/playground";
+import HomePage from "../pages/Home-page/HomePage";
 
 const NavbarRouter = () => {
-    return (
-        <Routes>
-            {/* Admin Login */}
+  return (
+    <Routes>
+      {/* Admin Login */}
 
-            {/* Navbar Layout Router */}
-            <Route path="/" element={<Navbar />}>
-                <Route index element={<h1>Hi Iam Home page</h1>} />
-                <Route path="/playground" element={<PlayGround />} />
-                <Route path="login" element={<Login />} />
-                <Route path="signup" element={<SignUp />} />
-                <Route path="/verify-profile/:verificationId/:code/:userId" element={<Verification />} />
-                <Route path="/successfully-profile/:successfullyId" element={<Successfully />} />
-                {/* navbar layout Private route */}
-                <Route element={<PrivateRoute />}>
-                    <Route path=":id" element={<h1>Hi Iam abla page</h1>} />
-                    <Route path="about" element={<h1>Hi Iam about page</h1>} />
-                    <Route path="create-card" element={<CreateCardPage />} />
-                    <Route path="/card-status/:cardId" element={<CardStatus />} />
-                    <Route path="faq" element={<FaqPage />} />
-                </Route>
-            </Route>
+      {/* Navbar Layout Router */}
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<HomePage />} />
+        <Route path="/playground" element={<PlayGround />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route
+          path="/verify-profile/:verificationId/:code/:userId"
+          element={<Verification />}
+        />
+        <Route
+          path="/successfully-profile/:successfullyId"
+          element={<Successfully />}
+        />
+        {/* navbar layout Private route */}
+        <Route element={<PrivateRoute />}>
+          <Route path=":id" element={<h1>Hi Iam abla page</h1>} />
+          <Route path="about" element={<h1>Hi Iam about page</h1>} />
+          <Route path="create-card" element={<CreateCardPage />} />
+          <Route path="/card-status/:cardId" element={<CardStatus />} />
+          <Route path="faq" element={<FaqPage />} />
+        </Route>
+      </Route>
 
-            {/* Dashboard Layout Router */}
-            <Route path="/admin" element={<DashboardLayout />}>
-                <Route path="profile-request/" element={<ProfileRequest />}>
-                    <Route path="all-user-profile" element={<AllRequest />} />
-                    <Route path="profile-creation/:profileId" element={<ProfileCreation />} />
-                </Route>
+      {/* Dashboard Layout Router */}
+      <Route path="/admin" element={<DashboardLayout />}>
+        <Route path="profile-request/" element={<ProfileRequest />}>
+          <Route path="all-user-profile" element={<AllRequest />} />
+          <Route
+            path="profile-creation/:profileId"
+            element={<ProfileCreation />}
+          />
+        </Route>
 
-                <Route path="card-request/" element={<CardRequest />}>
-                    <Route path="all/" element={<AllCard />} />
-                    <Route path="preview/:cardId" element={<SingleCardPreview />} />
-                </Route>
-                <Route path="login" element={<AdminLogin />} />
-            </Route>
-        </Routes>
-    );
+        <Route path="card-request/" element={<CardRequest />}>
+          <Route path="all/" element={<AllCard />} />
+          <Route path="preview/:cardId" element={<SingleCardPreview />} />
+        </Route>
+        <Route path="login" element={<AdminLogin />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default NavbarRouter;
